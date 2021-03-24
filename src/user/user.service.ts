80a7443cfc83c1868b5dc54e5b591ccc29ef430e
Teerasej <teerasej@gmail.com>
2021-03-24 22:01:35 +0700

    Inject model ของ User ที่สร้างไว้ เป็นตัวแทนของ Collection User ใน Database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { UserDto } from 'src/model/user-dto';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
 export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
     create(user:UserDto) {
 
