06fb78511dff9a7d24b4f1e92146579e052a4357
Teerasej <teerasej@gmail.com>
2021-03-24 21:39:07 +0700

    สร้าง signup route path ใช้ Class DTO รับข้อมูล JSON จาก request ในตอนนี้ให้ return ข้อมูล signup กลับออกไปก่อน

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserDto } from './model/user-dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('signup')
+  signup(@Body() user:UserDto) {
+    return user;
   }
 }
