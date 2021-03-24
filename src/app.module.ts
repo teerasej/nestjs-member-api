923ffea1c7c8757ed1137d93ab12086f86f6c3bb
Teerasej <teerasej@gmail.com>
2021-03-24 22:16:59 +0700

    สร้าง module และ service สำหรับส่วน authentication nest g module auth nest g service auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
