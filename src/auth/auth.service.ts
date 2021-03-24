0b8479f2c078e20b1ab2af3dd6d20533b3908b70
Teerasej <teerasej@gmail.com>
2021-03-24 23:43:07 +0700

    กำหนดค่า payload สำหรับการสร้าง token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
       }
 
       async login(user: any) {
-        const payload = { username: user.email };
+        const payload = { user: user.email };
         return {
           access_token: this.jwtService.sign(payload),
         };
