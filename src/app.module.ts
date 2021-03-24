f7ed00b0e7b3521a49e8d1b72d1cc5bf96d4794f
Teerasej <teerasej@gmail.com>
2021-03-24 21:57:38 +0700

    สร้าง module สำหรับ user โดยเฉพาะ มีการกำหนด UserDocument Type และ UserSchema และ Export UserService เพื่อให้สามารถเรียกใช้ได้ใน module อื่น

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
